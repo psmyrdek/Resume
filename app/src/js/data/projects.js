cd6c3091ab731f34ada0de52072d8d7e5328cbf6
Przemek <przemek.smyrdek@gmail.com>
2016-01-27 19:26:29 +0100

    updated projects

--- app/src/js/data/projects.js
+++ app/src/js/data/projects.js
@@ -4,19 +4,35 @@ var ProjectsModule = (function(module){
         {
             date: '03.2015',
             title: 'Coordinator',
-            award: '3 miejsce w Polsce w konkursie Microsoft Imagine Cup',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. '
+            award: true,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
+            features: ['.NET', 'AngularJS', 'SignalR'],
+            website: 'http://www.coordinator.pl'
         },
         {
             date: '03.2015',
-            title: 'Coordinator',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. '
+            title: 'Untwisted',
+            award: false,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
+            features: ['.NET', 'Real-Time'],
+            contributors: [
+                {
+                    name: 'Jacek',
+                    profile: 'http://www.onet.pl'
+                },
+                {
+                    name: 'Ania',
+                    profile: 'http://www.onet.pl'
+                },
+            ]
         },
         {
             date: '03.2015',
-            title: 'Coordinator',
-            award: '3 miejsce w Polsce w konkursie Microsoft Imagine Cup',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. '
+            title: 'IT Learning',
+            award: true,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
+            features: ['.NET', 'WebAPI', 'ReactJS'],
+            website: 'www.coordinator.pl'
         }
     ];
     
