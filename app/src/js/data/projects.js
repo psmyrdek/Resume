68f2e7c29ee025ff12ca84631261d82605911f61
Przemek <przemek.smyrdek@gmail.com>
2016-02-02 17:55:56 +0100

    removed margin style from tech-stack section

--- app/src/js/data/projects.js
+++ app/src/js/data/projects.js
@@ -3,7 +3,7 @@ var ProjectsModule = (function(module){
     module.list = [
         {
             date: '03.2015',
-            title: 'Coordinator',
+            title: 'IT Learning',
             award: true,
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
             features: ['.NET', 'AngularJS', 'SignalR'],
