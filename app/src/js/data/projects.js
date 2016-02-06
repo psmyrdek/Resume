a7bae0f76ab93b9c2cfb897a91088f8348d7ada4
Przemek <przemek.smyrdek@gmail.com>
2016-02-06 14:39:58 +0100

    updated projects structure

--- app/src/js/data/projects.js
+++ app/src/js/data/projects.js
@@ -2,7 +2,14 @@ var ProjectsModule = (function(module){
     
     module.list = [
         {
-            date: '03.2015',
+            date: '02.2016',
+            title: 'Web Resume',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
+            features: ['SCSS', 'Vue.JS', 'Gulp'],
+            website: 'http://www.coordinator.pl'
+        },
+        {
+            date: '08.2015',
             title: 'IT Learning',
             award: true,
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
@@ -10,8 +17,48 @@ var ProjectsModule = (function(module){
             website: 'http://www.coordinator.pl'
         },
         {
+            date: '06.2015',
+            title: 'WMS - Warehouse management',
+            award: true,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
+            features: ['.NET', 'AngularJS', 'SignalR'],
+            website: 'http://www.coordinator.pl'
+        },
+        {
             date: '03.2015',
+            title: 'NAV24',
+            award: true,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
+            features: ['.NET', 'AngularJS', 'SignalR'],
+            website: 'http://www.coordinator.pl'
+        },
+        {
+            date: '02.2015',
             title: 'Untwisted',
+            award: true,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
+            features: ['.NET', 'AngularJS', 'SignalR'],
+            website: 'http://www.coordinator.pl'
+        },
+        {
+            date: '01.2015',
+            title: 'Client\'s Dashboard',
+            award: true,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
+            features: ['.NET', 'AngularJS', 'SignalR'],
+            website: 'http://www.coordinator.pl'
+        },
+        {
+            date: '11.2014',
+            title: 'Rzeźbiarstwo.pl',
+            award: true,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
+            features: ['.NET', 'AngularJS', 'SignalR'],
+            website: 'http://www.coordinator.pl'
+        },
+        {
+            date: '09.2014',
+            title: 'Coordinator',
             award: false,
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
             features: ['.NET', 'Real-Time'],
@@ -25,14 +72,6 @@ var ProjectsModule = (function(module){
                     profile: 'http://www.onet.pl'
                 },
             ]
-        },
-        {
-            date: '03.2015',
-            title: 'IT Learning',
-            award: true,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare nec urna sit amet eleifend. Nam non tellus sed leo rhoncus blandit eget sed nibh. Etiam vel dolor lectus. Nam elementum placerat accumsan. Pellentesque lacinia neque at diam ornare, ut molestie tortor tempus. ',
-            features: ['.NET', 'WebAPI', 'ReactJS'],
-            website: 'www.coordinator.pl'
         }
     ];
     
