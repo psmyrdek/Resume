e5655f40a805c3f701a187149a58e4c4b161a162
Przemek <przemek.smyrdek@gmail.com>
2016-02-17 22:48:58 +0100

    new menu

--- app/src/js/vue-components.js
+++ app/src/js/vue-components.js
@@ -3,8 +3,22 @@
 var projectsComponent = new Vue({
     
     el: '#projects',
-    data : {
+    data: {
         projects: ProjectsModule.list
     }  
+    
+});
+
+var navbarComponent = new Vue({
+   
+   el: '#navbar',
+   data: {
+       isMenuVisible: false
+   },
+   methods: {
+       toggleMenu: function() {
+           this.isMenuVisible = !this.isMenuVisible;
+       }
+   }
     
 });
