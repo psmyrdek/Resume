b681e10f28ff95457400b2a337b8c250a8755556
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2017-07-09 00:04:13 +0200

    basic caching

--- sw.js
+++ sw.js
@@ -1,3 +1,29 @@
+var CACHE_NAME = 'resume-cache';
+var urlsToCache = [
+  '/',
+  '/app/dist/js/lib-scripts.min.js',
+  '/app/src/js/data/projects.js',
+  '/app/dist/js/app-scripts.min.js'
+];
+
 self.addEventListener('install', function(event) {
-    console.log('installer');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
