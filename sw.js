c14b228a12a14380827a91ef004842a8d73390a6
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2017-07-17 21:33:58 +0200

    sw registration

--- sw.js
+++ sw.js
@@ -1,30 +1,22 @@
-var CACHE_NAME = 'resume-cache';
-var urlsToCache = [
-  '/Resume/',
-  '/Resume/?utm_source=homescreen',
-  '/Resume/app/dist/js/lib-scripts.min.js',
-  '/Resume/app/src/js/data/projects.js',
-  '/Resume/app/dist/js/app-scripts.min.js'
-];
+var CACHE_NAME = 'app-cache';
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
+var urlsToCache = [ ];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+    return cache.addAll(urlsToCache);
+  }));
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       }
+//     )
+//   );
+// });
