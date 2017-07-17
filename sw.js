df9adeb8fca61aa789f5219faf66361e24979870
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2017-07-17 22:09:44 +0200

    cached responses

--- sw.js
+++ sw.js
@@ -13,15 +13,8 @@ self.addEventListener('install', (event) => {
   }));
 });
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         if (response) {
-//           return response;
-//         }
-//         return fetch(event.request);
-//       }
-//     )
-//   );
-// });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((resp) => {
+    return response ? response : fetch(event.request);
+  }));
+});
