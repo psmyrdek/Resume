faf29a28d8b8845b1608341e0aa53b233f31e5c1
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2017-07-19 22:16:40 +0200

    toolbelt and prod build

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,54 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-    entry: {
-        app: './src/index.js',
-        lib: './src/lib.js'
-    },
-    output: {
-        publicPath: 'dist/',
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            Vue: 'vue'
-        }),
-        new webpack.optimize.UglifyJsPlugin({})
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader?name=images/[name].[ext]'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader?name=fonts/[name].[ext]'
-                ]
-            }
-        ]
-    }
-}
+const merge = require('webpack-merge');
+
+module.exports = function (env) {
+
+    const baseConfig = {
+        entry: {
+            app: './src/index.js',
+            lib: './src/lib.js'
+        },
+        output: {
+            publicPath: 'dist/',
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].bundle.js'
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                Vue: 'vue'
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    use: [
+                        'file-loader?name=images/[name].[ext]'
+                    ]
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/,
+                    use: [
+                        'file-loader?name=fonts/[name].[ext]'
+                    ]
+                }
+            ]
+        }
+    };
+
+    const envConfig = require(`./config/webpack-${env}.config.js`);
+
+    return merge(baseConfig, envConfig);
+
+};
